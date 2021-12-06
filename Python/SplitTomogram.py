2df41a228a0915ea82eec5cd181887f1b9f5b1ed
Matt Larson <larsonmattr@gmail.com>
2021-12-06 12:37:40 -0600

    Bugfixes for SplitTomogram.py

--- Python/SplitTomogram.py
+++ Python/SplitTomogram.py
@@ -10,6 +10,7 @@ description_text = 'SplitTomogram: split the tiled images from a tilt-series int
 ## MIT License
 ## 
 ## Copyright (c) 2021 Jae Yang, Matt Larson
+## Updated 12/6/2021
 ## 
 ## Permission is hereby granted, free of charge, to any person obtaining a copy
 ## of this software and associated documentation files (the "Software"), to deal
@@ -38,8 +39,11 @@ import subprocess
 
 def createTiltList(output_directory, basename, subtilt, startTilt, endTilt, stepTilt, tiltFile):
     ''' Create a userlist.txt file defining the files/tilts in the stack '''
+    # Calculate number of files, should be the first line of this userlist.txt
+    tiltCount = (endTilt - startTilt) / stepTilt + 1
     processing_directory = os.path.join(output_directory, basename + '_Processing/')
     f = open(tiltFile, 'w')
+    f.write(str(tiltCount) + '\n')
     for tilt in range(startTilt, endTilt+1, stepTilt):
         output_name = os.path.join(output_directory, basename + '_subtilt_' + str(subtilt) + '_' + str(tilt) + '.0.mrc')
         f.write(output_name + '\n')
@@ -50,23 +54,27 @@ def createRawTilt(startTilt, endTilt, stepTilt, rawTiltFile):
     ''' Create user.rawtlt '''
     f = open(rawTiltFile, 'w')
     for tilt in range(startTilt, endTilt+1, stepTilt):
-        f.write(tilt + '\n')
+        f.write(str(tilt) + '\n')
     f.close()
 
-def copySubtilt(basename, tilt, subtilt, input_directory, output_name):
+def copySubtilt(basename, tilt, index, input_directory, output_name):
     ''' Split out the files of an individual tilt '''
-    expected_name = basename + '_' + subtilt + '_*_' + str(tilt) + '.0.*.mrc'
+    expected_name = basename + '_*' + str(index) + '_' + str(tilt) + '.0.*.mrc'
     # copy any of the matching tilts where filenames include the basename and tilt angle.
     patterns = [ expected_name ]
     if tilt == 0:
         #  Provide an alternative name for the 0 tilt can be -0.0 or 0.0
-        patterns.append( basename + '_' + subtilt + '_*_-' + str(tilt) + '.0.*.mrc' )
-        
+        patterns.append( basename + '_*' + str(index) + '_-' + str(tilt) + '.0.*.mrc' )
+    
+    count = 0
     for filename in os.listdir(input_directory):
         for pattern in patterns:
-            if (fnmatch.fnmatch(filename, expected_name)):
+            if (fnmatch.fnmatch(filename, pattern)):
                 # store original frame in the stitching directory with expected filename.
-                shutil.copyfile(filename, output_name)
+                src = os.path.join(input_directory, filename)
+                shutil.copyfile(src, output_name)
+                count += 1
+    print("Copied " + str(count) + " files matching " + str(patterns) + " from " + input_directory + " to " + output_name)
 
 def main():
     # 1. Provide commandline options
@@ -77,21 +85,31 @@ def main():
     parser.add_argument('--output', help='path to a location to write results', required=True, default=None)
     parser.add_argument('--basename', help='define a common basename for the images', required=True, default=None)
     parser.add_argument('--period', help='define number of subtilts', type=int, required=True, default=None)
-    parser.add_argument('--first_subtilt', help='define first subtilt index', type=int, required=True, default=1)
-    parser.add_argument('--last_subtilt', help='define last subtilt index', type=int, required=True, default=9)
+    parser.add_argument('--seqnum_start', help='sequential starting number (000)', type=int, required=True, default=000)
+    parser.add_argument('--seqnum_end', help='sequential ending number (000)', type=int, required=True, default=000)
     parser.add_argument('--starting_angle', help='define the minimal bounds of the tilt range, ex. -60', type=int, required=True, default=None)
     parser.add_argument('--ending_angle', help='define the maximal bounds of the tilt range, ex. 60', type=int, required=True, default=None)
     parser.add_argument('--tilt_increment', help='define the increment of the tilt, ex 3', type=int, required=True, default=None)
     args = parser.parse_args()
 
+    # Need to separate X subtilts at each tilt angle into separate folder. This represents an offset.
+    window_start = args.seqnum_start
+
     ## Split the tilt files for each subtilt into a separate directory, and stack.
-    for subtilt in range(args.first_subtilt, args.last_subtilt, args.period):
-        outputTiltDir = os.path.join(args.output, 'subTilt_' + str(subtilt))
-        os.makedirs(outputTiltDir)
-        for tilt in range(args.starting_angle, args.ending_angle+1, args.tilt_increment):
+    for tilt in range(args.starting_angle, args.ending_angle+1, args.tilt_increment):
+        ## For each tilt angle, there are multiple 'subtilts' to separate into directories
+        for subtilt in range(1, args.period + 1):
+            outputTiltDir = os.path.join(args.output, 'subTilt_' + str(subtilt))
+            if (not os.path.isdir(outputTiltDir)):
+                os.makedirs(outputTiltDir)
+            index = window_start + subtilt - 1
             output_name = os.path.join(outputTiltDir, args.basename + '_subtilt_' + str(subtilt) + '_' + str(tilt) + '.0.mrc')
-            copySubtilt(args.basename, tilt, subtilt, args.input, output_name)
-        
+            copySubtilt(args.basename, tilt, index, args.input, output_name)
+        window_start += args.period
+
+    ## Build a stack in each subtilt directory.
+    for subtilt in range(1, args.period+1):   
+        outputTiltDir = os.path.join(args.output, 'subTilt_' + str(subtilt))   
         rawTiltTxt = os.path.join(outputTiltDir, 'rawtlt.txt')
         createRawTilt(args.starting_angle, args.ending_angle, args.tilt_increment, rawTiltTxt)
 
@@ -102,6 +120,8 @@ def main():
         finalStackFile = os.path.join(outputTiltDir, args.basename + '_subtilt_' + str(subtilt) + '_' + str(tilt) + '_AliSB.st')
 
         # Generate usertilt.txt
-        subprocess.run('newstack', '-tilt', rawTiltTxt, '-fileinlist', tiltListTxt, '-ou', finalStackFile)
+        subprocess.run(['newstack', '-tilt', rawTiltTxt, '-fileinlist', tiltListTxt, '-ou', finalStackFile])
 
 
+if __name__ == "__main__":
+    main()
